077f6f76d3952de6a03f7abdf8a975b000ae9f09
Vanessa Marcelino <vrmarcelino@gmail.com>
2021-03-04 13:10:39 +1100

    replaced a class obj with 2 dictionaries, made the script orders of magnitude faster

--- MetModels_rename_bin_headers.py
+++ MetModels_rename_bin_headers.py
@@ -35,78 +35,57 @@ output = args.output_fp
 #tax_fp = "gtdb_bac_and_arch.tsv"
 #output = "merged_renamed_db.fas"
 
-### Define a class object for each ctg header, which stores binID and taxonomy
-class CtgInfo():
-    def __init__(self, contigID=None, full_ctg_name=None, binID=None, taxonomy=None):
-
-        self.contigID = contigID
-        self.full_ctg_name = full_ctg_name
-        self.binID = binID
-        self.taxonomy = taxonomy
-
-### store info in a list of CtgInfo objects
-ctgs_info = []
+### make a dictionary with contigs 2 clusters data:
+ctg2bin = {}
 with open (wanted_clusters_fp) as clusters:
     for line in clusters:
         bin = line.split("\t")[0]
-        full_ctg_name = line.split("\t")[1].rstrip()
-        contigID = full_ctg_name.split("-")[0]
+        ctg = line.split("\t")[1].rstrip()
+        ctg2bin[ctg] = bin
 
-        ci = CtgInfo()
-        ci.contigID = contigID
-        ci.full_ctg_name = full_ctg_name
-        ci.binID = bin
-        ctgs_info.append(ci)
 
-### Store taxonomy in ctgs_info
+### make another dict bin2tax:
+bin2tax = {}
 with open (tax_fp) as tax:
     next(tax) # skip header
     for line in tax:
         bin = line.split("\t")[0]
         taxonomy = line.split("\t")[1]
         taxonomy = taxonomy.replace(" ", "_") # removes spaces from species names.
-
-        for c in ctgs_info:
-            if c.binID == bin:
-                c.taxonomy = taxonomy
-
-
-### function that takes as input a ctg header, and outputs the wanted ctg header format
-def beheader(old_header, list_of_ctgsInf, counter):
-    for x in list_of_ctgsInf:
-        if x.full_ctg_name == old_header:
-            new_header = x.binID + "|" + x.contigID + "|" + x.taxonomy
-            counter += 1
-            break
-        else:
-            new_header = "not_renamed_" + old_header + "\n"
-    return(new_header, counter)
+        bin2tax[bin] = taxonomy
 
 
 ### rename  contig headers, store in a new file:
 renamed_db = open(output, 'w')
-ctgs = 0
+ctgs_not_found = 0
 ctgs_renamed = 0
+ctgs = 0
 with open(in_fasta) as genome:
     for line in genome:
         if line.startswith(">"):
             ctgs += 1
-            contig_name = line.split(">")[1].strip()
-            rename_seq = beheader(contig_name, ctgs_info, ctgs_renamed)
-            if ctgs == 1: # does not need a new line in the beginning of the file
-                line = ">" + rename_seq[0]
-            else:
-                line = "\n>" + rename_seq[0]
-            ctgs_renamed = rename_seq[1]
+            contig_full_name = line.split(">")[1].strip()
 
-        else:
-            line = line.replace("\n", "") # convert interleaved to sequential
+            if contig_full_name in ctg2bin.keys():
+                binID = ctg2bin[contig_full_name]
+                tax = bin2tax[binID]
+                contigID = contig_full_name.split("-")[0]
+                new_header = binID + "|" + contigID + "|" + tax
+                ctgs_renamed += 1
+
+                if ctgs == 1:  # does not need a new line in the beginning of the file
+                    line = ">" + new_header
+                else:
+                    line = "\n>" + new_header
+            else:
+                ctgs_not_found += 1
+                line = "\n>" + contig_full_name + "\n"
+
+        else: # convert interleaved to sequential
+            line = line.replace("\n", "")
 
         renamed_db.write(line)
 
 renamed_db.close()
 
-ctgs_not_found = ctgs - ctgs_renamed
-
 print ("Done! %i contigs renamed, and %i contigs were not found" %(ctgs_renamed,ctgs_not_found))
-
