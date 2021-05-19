6fb558f7bce88f1aca346b52f43502f5800e6cc4
Vanessa Marcelino <vrmarcelino@gmail.com>
2021-05-19 14:15:29 +1000

    added snakemake scripts here

---
+++ Snakemake/4_MICOM/MICOM_Snakefile.py
@@ -0,0 +1,59 @@
+# build community models and obtain metabolic exchanges
+
+configfile: "MICOM_config.yaml"
+samples_fp = "0_MAGs_tables/all_samples.txt"
+
+
+# Process samples file
+#samples_list = open(config["path"]["root"]+config["samples"]["sam"]) # not working in PyCharm
+with open(samples_fp) as f:
+    samples = f.read().splitlines()
+
+
+rule all:
+    input:
+        expand(config["path"]["root"]+"/"+config["folder"]["tradeoffs"]+"/minimal_fluxes_exchange_{sample}.csv", sample = samples)
+
+
+rule build_community:
+    input:
+        sample_file = config["path"]["root"]+"/"+config["folder"]["tables_fp"]+"/{sample}.csv"
+    params:
+        smpl = "{sample}"
+    output:
+        config["path"]["root"]+"/"+config["folder"]["pickles"]+"/{sample}.pickle"
+    resources:
+        time_min=1440, mem_mb=20000, cpus=config["cores"]["build_comm"]
+    log:
+        std_out = config["path"]["root"]+"/"+config["folder"]["logs"]+"/micom_build_comm/{sample}.log"
+    shell:
+        """     
+        echo "Begin building commuities with MICOM ... "
+        python3 MICOM_build_comm_models.py -s {params.smpl}
+        
+        echo "Done"
+        """
+rule tradeoff:
+    input:
+        config["path"]["root"]+"/"+config["folder"]["pickles"]+"/{sample}.pickle"
+    params:
+        smpl = "{sample}.pickle",
+        out_folder = config["path"]["root"]+"/"+config["folder"]["tradeoffs"]
+    output:
+        config["path"]["root"]+"/"+config["folder"]["tradeoffs"]+"/minimal_fluxes_exchange_{sample}.csv"
+    resources:
+        time_min=1440, mem_mb=20000, cpus=config["cores"]["tradeoff"]
+    log:
+        std_out = config["path"]["root"]+"/"+config["folder"]["logs"]+"/micom_tradeoff_comm/{sample}.log"
+    shell:
+        """
+       
+        echo "Begin tradeoff analyses with MICOM ... "
+
+        python3 MICOM_coop_tradeoff.py -sn {params.smpl} -t {resources.cpus} -o {params.out_folder}
+        
+        echo "Done!"
+        """
+
+
+
