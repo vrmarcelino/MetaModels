0c1abf7366658daf611c46dc9534059d2506e4bb
Vanessa Marcelino <vrmarcelino@gmail.com>
2021-05-19 10:58:58 +1000

    enable using the tradeoff workflow with one sample at a time, suitable for a Snakemake workflow

--- MICOM_coop_tradeoff.py
+++ MICOM_coop_tradeoff.py
@@ -4,7 +4,7 @@
 Script to extract minimal growth media and optimize the cooperative tradeoff
 Run this after MICOM_build_comm_models.py
 Created on 30/3/21
-edited: 06/05/21
+edited: 19/05/21
 @author: V.R.Marcelino
 
 Script adapted from C. Diener's scripts:
@@ -12,6 +12,7 @@ https://github.com/micom-dev/paper
 """
 
 #imports
+import sys
 import os
 from argparse import ArgumentParser
 import pandas as pd
@@ -23,20 +24,23 @@ import time
 
 
 parser = ArgumentParser()
-parser.add_argument('-s', '--sample_list', help="""path to a txt file containing sample.pickle names""", required=True)
+parser.add_argument('-sl', '--sample_list', help="""path to a txt file containing sample.pickle names""", required=False)
+parser.add_argument('-sn', '--sample_name', help="""sample name (e.g. SRR413605.pickle). Required when doing analyses with one sample at time (e.g. with snakemake)""", required=False)
 parser.add_argument('-p', '--pickles_path', help="""path to the folder containing the pickle files. Default = 1_communities""", required=False, default="1_communities")
-parser.add_argument('-trad', '--trade_off', help="""trade_off (fration) to use in the cooperative tradeoff""", required=False, default=0.5)
+parser.add_argument('-trad', '--trade_off', help="""trade_off (fraction) to use in the cooperative tradeoff""", required=False, default=0.5)
 parser.add_argument('-t', '--threads', help="""number of threads. Default = 2""", required=False, default=2)
 parser.add_argument('-o', '--out_folder', help="""output_folder. Default = 2_TradeOffs""", required=False, default="2_TradeOffs")
 
 
 args = parser.parse_args()
 
-samples_fp = args.sample_list
+samples_list_fp = args.sample_list
+samples_name_fp = args.sample_name
 pickles_path = args.pickles_path
 trade_off = args.trade_off # fraction
 max_procs = int(args.threads)
 out_dir=args.out_folder
+
 
 #samples_fp='1_communities/individual_ERR_samples_test.txt'
 #pickles_path = '1_communities'
@@ -44,16 +48,27 @@ out_dir=args.out_folder
 #max_procs = 2
 #out_dir = "2_TradeOffs"
 
+# check if sample list or name were given:
+if samples_list_fp != None:
+    ## read sample names to a list
+    with open(samples_list_fp) as f:
+        samples = f.read().splitlines()
+    if 'samples.txt' in samples:
+        samples.remove('samples.txt') #there if the list was created with ls -1 > samples.txt
+
+elif samples_name_fp != None:
+    samples = [samples_name_fp]
+    print (samples)
+
+else:
+    print ("Error: please provide --sample_list or --sample_name.")
+    sys.exit()
+
 
 ## make out dir
 if not os.path.exists(out_dir):
     os.mkdir(out_dir)
 
-## read sample names to a list
-with open(samples_fp) as f:
-    samples = f.read().splitlines()
-if 'samples.txt' in samples:
-    samples.remove('samples.txt') #there if the list was created with ls -1 > samples.txt
 
 
 ## function that optimizes the cooperative tradeoff, first using the western media for upper boundaries,
