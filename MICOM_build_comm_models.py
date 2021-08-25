cc0f35db56d468c1f0ae4f6c4c13461854d44437
Vanessa Marcelino <vrmarcelino@gmail.com>
2021-08-25 13:39:42 +1000

    updated build comm models script to be compatible with MICOM v0.25.1

--- MICOM_build_comm_models.py
+++ MICOM_build_comm_models.py
@@ -3,11 +3,14 @@
 """
 Script to run MICOM and build community models.
 Also adds a media to the community and saves it as a pickle file.
-Using here the Western Diet csv file, formated to work with carveme, from the MICOM website (thanks Chris!)
+Using here the Western Diet csv file, formated to work with carveme the MICOM website,
 which also includes the dilution factors that lower import flux bounds for metabolites that are commonly
 absorbed in the small intestine
 
+Compatible with MICOM v.0.25.1
+
 Created on 06/05/21
+Updated 25/08/21
 @author: V.R.Marcelino
 """
 import os
@@ -36,10 +39,11 @@ comm_folder = args.pickles
 sample = args.sample
 w_media = args.media
 
-#in_folder = '0_MAGs_tables'
-#comm_folder = '1_communities'
-#sample = 'test_sample'
-#w_media = '0_diet/western_diet_gut_carveme.qza'
+
+in_folder = '0_MAGs_tables'
+comm_folder = '1_communities'
+sample = 'ERR589448'
+w_media = '0_diet/western_diet_gut_carveme.qza'
 
 if not os.path.exists(comm_folder):
     os.mkdir(comm_folder)
@@ -58,7 +62,7 @@ medium = load_qiime_medium(w_media)
 
 # In order to convert the specification in a community model we will use the Community class from micom
 # which derives from the cobrapy Model class.
-# this took ~45 sec for 10 MAGs
+# this took 15min for a sample
 print ("\nBuilding community, be patient...\n")
 com = Community(mag_tb)
 print("Done. Built a community with a total of {} reactions.\n".format(len(com.reactions)))
@@ -69,24 +73,17 @@ print ("Adding media and saving to pickle\n")
 # check if the names match & add to the community object:
 ex_ids = [r.id for r in com.exchanges]
 
-# replace _m with _e_m (see what needs to change with the --fbc2 models)
-medium = medium.replace(regex=r'_m$', value='_e_m')
-
 medium['reaction'].isin(ex_ids).sum() # must be a large number of reactions (>100)
-medium.index = medium.index.str.replace(r'_m$','_e_m', regex=True)
 med = medium[medium.index.isin(ex_ids)] # exclude medium items not used by the microbiome
 
 diet = med.flux * med.dilution # dilute nutrients absorbed in the small intestine
 
-
-# This only affects the com.cooperative_tradeoff (the grow workflow is not affected)
+# Note that this only affects the com.cooperative_tradeoff (the grow workflow is not affected)
 com.medium = diet
 
-
-# save this community to file 
+# save this community to file
 comm_fp = comm_folder + "/" + sample + ".pickle"
 com.to_pickle(comm_fp)
 
 print ("\nDone building community model for %s!\n"%(sample))
-
 
